840b3e86b812aecaf58b23b22e7dde23960b3b89
zaidra <zaid.sarc@gmail.com>
2021-01-03 23:53:12 +0200

    add getAllValidProduct.php

---
+++ SRC/model/getAllValidProduct.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once("../dataSources/config/connectWithRemoteDB.php");
+session_start();
+$connect = new DbConnection();
+$sql = "SELECT * FROM `product`where `productState` = 'Validate'";
+$isParent = $_SESSION['parent'];
+if ($isParent == 'Users') {
+    $result = mysqli_query($connect->getdbconnect(), $sql);
+    $msg = "";
+    if ($result) {
+        // echo "Order Shipped";
+        $msg = 1;
+    } else {
+        echo mysqli_error($connect->getdbconnect());
+        $msg = 2;
+    }
+}
+// header("Location: ../Pages/seller-product.php?msg=$msg");
+// must forwarding to unshipped Orders page
+mysqli_close($connect->getdbconnect());
