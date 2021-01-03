d863cfc789161c0eef17909534c24dcc4a6e39c4
MohammadAlhariri <52330834+MohammadAlhariri@users.noreply.github.com>
2021-01-03 17:00:50 +0200

    add product to header updated2

---
+++ SRC/dataSources/config/login.php
@@ -0,0 +1,27 @@
+<?php
+require("../config/connectWithRemoteDB.php");
+include("../../model/getUserInformationByPhone.php");
+$phone = addslashes(strip_tags($_POST['form-phone']));
+$password = addslashes(strip_tags($_POST['form-password']));
+// $parent = addslashes(strip_tags($_POST['parent']));
+$connect =new DbConnection();
+$sql = "SELECT `userID`, `userName`, `userPhone`, `userEmail`, `userAddress`, `userPassword`, `userImage`, `userAnswer1`, `userAnswer2`,`parent`  
+    FROM `user` WHERE `userPhone`= $phone AND `parent` ='Users';";
+if ($result = mysqli_query($connect->getdbconnect(), $sql)) {
+    $emparray = array();
+    $row = mysqli_fetch_array($result);
+
+    if (!empty($row)) {
+        if ($row["userPassword"] == $password) {
+            getUserInfo($phone);
+            header("Location: ../../Pages/index.php");
+        } else {
+            header("Location: ../../Pages/welcome-page-user.php?msg=1");
+            // password Wrong
+        }
+
+    } else {
+        header("Location: ../../Pages/welcome-page-user.php?msg=2");
+//            user not found
+    }
+}
---
+++ SRC/dataSources/config/register.php
@@ -0,0 +1,25 @@
+<?php
+
+require("../config/connectWithRemoteDB.php");
+include("../../model/getUserInformationByPhone.php");
+$connect = new DbConnection();
+$cid = addslashes(strip_tags($_POST['form-phone']));
+$fname = addslashes(strip_tags($_POST['form-first-name']));
+$lname = addslashes(strip_tags($_POST['form-last-name']));
+$key = addslashes(strip_tags($_POST['form-password']));
+$fullName = $fname . " " . $lname;
+$sql = "INSERT INTO `user` (`userName`, `userPhone`,`userPassword`, `parent`) VALUES ( '$fullName', $cid, '$key','Users')";
+
+$result = mysqli_query($connect->getdbconnect(), $sql);
+
+
+if ($result) {
+
+    session_start();
+    // Session Variables are created
+    getUserInfo($cid);
+    header("Location: ../../Pages/index.php");
+
+} else {
+    header("Location: ../../Pages/welcome-page-seller.php");
+}
