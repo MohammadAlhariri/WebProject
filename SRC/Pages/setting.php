06fcba2bf093fc16adfc2c3dadf41aac82a414a4
MohammadAlhariri <52330834+MohammadAlhariri@users.noreply.github.com>
2021-01-06 18:13:35 +0200

    change profile image error

--- SRC/Pages/setting.php
+++ SRC/Pages/setting.php
@@ -7,15 +7,17 @@ include "IncludesParts/header.php";
         <div class="row">
             <div class="col-md-4 border-right">
                 <div class="d-flex flex-column align-items-center text-center p-3 py-5">
-                    <a id="addFile"  onclick="document.getElementById('fileInput').click();"><i class="fa fa-camera"></i></a>
-                    <img id="imgLogo" class="rounded-circle mt-5" src="../<?php echo $_SESSION["userImage"]; ?>" width="150"><span
-                            class="font-weight-bold">John Doe</span><span
-                            class="text-black-50">john_doe12@bbb.com</span><span>United States</span></div>
+
+
+                    <img id="imgLogo" class="rounded-circle mt-5" src="<?php echo $_SESSION["userImage"]; ?>"
+                         onclick="document.getElementById('fileInput').click();"
+                         width="250">
+                    <h3 class="p-2">Please click on profile photo to change it </h3></div>
             </div>
             <div class="col-md-8">
                 <div class="p-3 py-5">
 
-                    <form action="">
+                    <form action="../model/updateUserProfile.php" method="post" enctype="multipart/form-data">
                         <div class="d-flex justify-content-between align-items-center mb-3">
                             <div class="d-flex flex-row align-items-center back"><i
                                         class="fa fa-long-arrow-left mr-1 mb-1"></i>
@@ -23,34 +25,82 @@ include "IncludesParts/header.php";
                             </div>
                             <h6 class="text-right">Edit Profile</h6>
                         </div>
-                        <div class="row mt-2">
-                            <div class="col-md-6"><label for="name">Full Name</label></div>
-                            <div class="col-md-6"><input id="Name" name="name" type="text" class="form-control"
-                                                         value="<?php echo $_SESSION["userName"]; ?>"
-                                                         placeholder="Enter Full Name"></div>
-                        </div>
-                        <div class="row mt-3">
-                            <div class="col-md-6"><label for="phone">Phone</label></div>
-                            <div class="col-md-6"><input id="Phone" name="phone" type="number" class="form-control"
-                                                         value="<?php echo $_SESSION["userPhone"]; ?>"
-                                                         placeholder="Enter Phone number"></div>
-                        </div>
-                        <div class="row mt-3">
-                            <div class="col-md-6"><label for="email">Email address</label></div>
-                            <div class="col-md-6"><input id="Email" name="email" type="email" class="form-control"
-                                                         value="<?php echo $_SESSION["userEmail"]; ?>"
-                                                         placeholder="Enter Email"></div>
-                        </div>
-                        <div class="row mt-3">
-                            <div class="col-md-6"><label for="address">Address</label></div>
-                            <div class="col-md-6"><input id="Address" name="address" type="text" class="form-control"
-                                                         value="<?php echo $_SESSION["userAddress"]; ?>"
-                                                         placeholder="Enter Address"></div>
-                        </div>
-                        <input name="fileInput" id="fileInput" type="file" style="position: absolute;top: 20000px;">
-                        <div class="mt-5 text-right">
-                            <button name="submit" class="btn btn-primary profile-button" type="button">Save Profile
-                            </button>
+                        <?php if ($_SESSION["parent"] == "Users" || $_SESSION["parent"] == "Admins") { ?>
+                            <div class="row mt-2">
+                                <div class="col-md-6"><label for="name">Full Name</label></div>
+                                <div class="col-md-6"><input id="name" name="name" type="text" class="form-control"
+                                                             value="<?php echo $_SESSION["userName"]; ?>"
+                                                             placeholder="Enter Full Name"></div>
+                            </div>
+
+
+                            <div class="row mt-3">
+                                <div class="col-md-6"><label for="phone">Phone</label></div>
+                                <div class="col-md-6"><input disabled id="phone" name="phone" type="number"
+                                                             class="form-control"
+                                                             value="<?php echo $_SESSION["userPhone"]; ?>"
+                                                             placeholder="Enter Phone number"></div>
+                            </div>
+                            <div class="row mt-3">
+                                <div class="col-md-6"><label for="email">Email address</label></div>
+                                <div class="col-md-6"><input id="email" name="email" type="email" class="form-control"
+                                                             value="<?php echo $_SESSION["userEmail"]; ?>"
+                                                             placeholder="Enter Email"></div>
+                            </div>
+
+
+
+                            <div class="row mt-3">
+                                <div class="col-md-6"><label for="address">Address</label></div>
+                                <div class="col-md-6"><input id="address" name="address" type="text"
+                                                             class="form-control"
+                                                             value="<?php echo $_SESSION["userAddress"]; ?>"
+                                                             placeholder="Enter Address"></div>
+                            </div>
+                        <?php } else if ($_SESSION["parent"] == "seller") { ?>
+
+                            <div class="row mt-2">
+                                <div class="col-md-6"><label for="name">Full Name</label></div>
+                                <div class="col-md-6"><input id="name" name="name" type="text" class="form-control"
+                                                             value="<?php echo $_SESSION["sellerName"]; ?>"
+                                                             placeholder="Enter Full Name"></div>
+                            </div>
+
+
+                            <div class="row mt-3">
+                                <div class="col-md-6"><label for="phone">Phone</label></div>
+                                <div class="col-md-6"><input disabled id="phone" name="phone" type="number"
+                                                             class="form-control"
+                                                             value="<?php echo $_SESSION["sellerPhone"]; ?>"
+                                                             placeholder="Enter Phone number"></div>
+                            </div>
+                            <div class="row mt-3">
+                                <div class="col-md-6"><label for="email">Email address</label></div>
+                                <div class="col-md-6"><input id="email" name="email" type="email" class="form-control"
+                                                             value="<?php echo $_SESSION["sellerEmail"]; ?>"
+                                                             placeholder="Enter Email"></div>
+                            </div>
+
+
+
+                            <div class="row mt-3">
+                                <div class="col-md-6"><label for="address">Address</label></div>
+                                <div class="col-md-6"><input id="address" name="address" type="text"
+                                                             class="form-control"
+                                                             value="<?php echo $_SESSION["sellerAddress"]; ?>"
+                                                             placeholder="Enter Address"></div>
+                            </div>
+
+                        <?php } ?>
+                        <input name="fileInput" id="fileInput" type="file" accept="image/*"
+                               style="position: absolute;top: 20000px;">
+                        <div class="row">
+                            <div class="col-md-2">
+                                <button name="submit" type="submit" class="btn btn-primary text-uppercase">Update
+                                    Profile
+                                </button>
+                            </div>
+
                         </div>
                     </form>
 
